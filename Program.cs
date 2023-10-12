2a85e51b96fadcc806817eaad50923a75e2711ec
NIKKnak <nik88350759@yandex.ru>
2023-10-12 16:28:11 +0300

    first commit

--- Program.cs
+++ Program.cs
@@ -4,6 +4,7 @@
     {
         static void Main(string[] args)
         {
+            //DZ-1
             if (args.Length == 3)
             {
                 int result = 0;
@@ -30,6 +31,35 @@
             }
             else
                 Console.WriteLine("Для использования программы введите: число знак(+-/*) число");
+
+
+
+            //DZ-2
+            int[,] a = { { 7, 3, 2 }, { 4, 9, 6 }, { 1, 8, 5 } };
+            int y = a.GetLength(0);
+            int x = a.GetLength(1);
+            int[] sort = new int[x * y];
+            int counter = 0;
+            for (int i = 0; i < y; i++)
+            {
+                for (int j = 0; j < x; j++)
+                {
+                    sort[counter] = a[i, j];
+                    counter++;
+                }
+            }
+            Array.Sort(sort);
+            counter = 0;
+            for (int i = 0; i < y; i++)
+            {
+                for (int j = 0; j < x; j++)
+                {
+                    a[i, j] = sort[counter];
+                    counter++;
+                    Console.Write(a[i, j] + " ");
+                }
+                Console.WriteLine();
+            }
         }
 
     }
